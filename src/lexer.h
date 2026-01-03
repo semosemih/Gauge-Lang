f89bfccc0c69759960983586d6345c1c20248e39
semosemih <123261590+semosemih@users.noreply.github.com>
2026-01-03 21:07:13 +0300

    lexer implementation

---
+++ src/lexer.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "token.h"
+
+class Lexer{
+public:
+    explicit Lexer(const std::string& source);
+
+    std::vector<Token> scanTokens();
+private:
+    const std::string source;
+    std::vector<Token> tokens;
+
+    int start = 0 ;
+    int current = 0;
+    int line = 1;
+
+    void scanToken();
+    char advance();
+    bool isAtEnd() const;
+
+    void addToken(TokenType type);
+};
