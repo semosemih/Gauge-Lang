80d3805f52caaf9d00f5f64b85725e9780adb0fd
semosemih <123261590+semosemih@users.noreply.github.com>
2026-01-04 23:52:30 +0300

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "lexer.h"
+
+int main(){
+    std::string source = "let x = 5 + 3;";
+    Lexer lexer(source);
+
+    auto tokens = lexer.scanTokens();
+    for(const auto& token : tokens){
+        std::cout << static_cast<int>(token.type)
+            << " : " << token.lexeme << std::endl;
+    }
+
+    return 0;
+}
